b9108ce69b26970645d08055a354c1928673bfb7
Fukalov Sem <fukalov@sibvaleo.com>
2016-08-23 10:47:32 +0700

    Added composer support and namespace

--- qrspec.php
+++ qrspec.php
@@ -30,7 +30,9 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  */
- 
+
+namespace PHPQRCode;
+
     define('QRSPEC_VERSION_MAX', 40);
     define('QRSPEC_WIDTH_MAX',   177);
 
